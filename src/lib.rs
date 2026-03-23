56250b84df9af90e94f72e088a7c521a7599b536
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2026-03-23 22:57:11 +0100

    Improve MIDI loopback error handling and add event logging in process method

--- src/lib.rs
+++ src/lib.rs
@@ -31,13 +31,12 @@ impl Plugin for LoopbackMidiPlugin {
     const URL: &'static str = "";
     const EMAIL: &'static str = "";
     const VERSION: &'static str = "0.0.2";
-    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
-        AudioIOLayout {
-            main_input_channels: None,
-            main_output_channels: NonZeroU32::new(2),
-            ..AudioIOLayout::const_default()
-        },
-    ];    const MIDI_INPUT: MidiConfig = MidiConfig::MidiCCs;
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
+        main_input_channels: None,
+        main_output_channels: NonZeroU32::new(2),
+        ..AudioIOLayout::const_default()
+    }];
+    const MIDI_INPUT: MidiConfig = MidiConfig::MidiCCs;
     type SysExMessage = ();
     type BackgroundTask = ();
 
@@ -53,7 +52,10 @@ impl Plugin for LoopbackMidiPlugin {
     ) -> bool {
         self.midi_loopback = match MIDILoopback::new("NewLoopback") {
             Ok(loopback) => Some(loopback),
-            Err(_err) => return false,
+            Err(err) => {
+                nih_log!("Failed to create MIDI loopback: {}", err);
+                return false;
+            }
         };
         self.params.loopback_name.set("NewLoopback".to_string());
         true
@@ -63,16 +65,25 @@ impl Plugin for LoopbackMidiPlugin {
         &mut self,
         _buffer: &mut Buffer,
         _aux: &mut AuxiliaryBuffers,
-        _context: &mut impl ProcessContext<Self>,
+        context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
+        while let Some(event) = context.next_event() {
+            while let Some(event) = context.next_event() {
+                match event {
+                    NoteEvent::NoteOn { timing, note, velocity, channel, .. } => {
+                        // timing = sample offset from start of this buffer
+                        nih_log!("Note ON at sample {}: ch={} note={}", timing, channel, note);
+                    }
+                    _ => {}
+                }
+            }
+        }
         ProcessStatus::KeepAlive
     }
 }
 impl Vst3Plugin for LoopbackMidiPlugin {
     const VST3_CLASS_ID: [u8; 16] = *b"AZLoopbackMIDI\x01\x01";
-    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] = &[
-        Vst3SubCategory::Tools,
-        Vst3SubCategory::Instrument,
-    ];
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Tools, Vst3SubCategory::Instrument];
 }
 nih_export_vst3!(LoopbackMidiPlugin);
